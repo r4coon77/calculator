1a9f3e351dd705f6f87dd9a39782bc42622101e9
r4coon77 <139823112+r4coon77@users.noreply.github.com>
2024-09-14 16:43:53 +0100

    Added functions for add, multiple, subtract and divide

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log("hello")
+
+function addFunction(num1, num2) {
+    result = parseInt(num1) + parseInt(num2);
+    console.log(result)
+}
+
+function multiplyFunction(num1, num2) {
+    result = parseInt(num1) * parseInt(num2);
+    console.log(result)
+}
+
+function subtractFunction(num1, num2) {
+    result = parseInt(num1) - parseInt(num2);
+    console.log(result)
+}
+
+function divideFunction(num1, num2) {
+    result = parseInt(num1) / parseInt(num2);
+    console.log(result)
+}
+
+
+
+
+
