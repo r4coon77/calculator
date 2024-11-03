3d558c8966a3138d66c2a8abf1d49e5fce73df4e
r4coon77 <139823112+r4coon77@users.noreply.github.com>
2024-11-03 21:05:34 +0000

    Added event listener when = is pressed, which calls addFunction. Also added function clears firstNum and secondNum and opetaror variables when the Clear button is clicked

--- app.js
+++ app.js
@@ -11,21 +11,26 @@ function operateFunction(num1, num2) {
 function addFunction(num1, num2) {
     result = parseInt(num1) + parseInt(num2);
     console.log(result)
+    displayScreen.querySelector("div").innerText = result;
+    return result
 }
 
 function multiplyFunction(num1, num2) {
     result = parseInt(num1) * parseInt(num2);
     console.log(result)
+    displayScreen.querySelector("div").innerText = result;
 }
 
 function subtractFunction(num1, num2) {
     result = parseInt(num1) - parseInt(num2);
     console.log(result)
+    displayScreen.querySelector("div").innerText = result;
 }
 
 function divideFunction(num1, num2) {
     result = parseInt(num1) / parseInt(num2);
     console.log(result)
+    displayScreen.querySelector("div").innerText = result;
 }
 
 
@@ -35,49 +40,61 @@ let displayScreen = document.getElementById("display-screen");
 let numPadButtons = document.querySelectorAll(".nm-btn");
 let equalButton = document.querySelectorAll(".eq-btn");
 
-// for(i of numPadButtons) {
-//     i.addEventListener('click', function(e) {
-//     e.stopPropagation()    
-//     console.log(e.target.innerText)
-//     displayNumInput = e.target.innerText;
-//     displayScreen.querySelector("div").innerText = displayNumInput;
-// });
-// }
-
+function clearCalc() {
+    firstNum = "";
+    secondNum = "";
+    operator = "";
+    displayScreen.querySelector("div").innerText = 0;
+}
 
 let firstNum = "";
 let secondNum = "";
 let operator = "";
 
-
 for(i of numPadButtons) {
     i.addEventListener('click', function(digit) {
-    digit.stopPropagation()   
+    digit.stopPropagation() 
+    if(operator === "" && secondNum === "") { 
     firstNum = firstNum + digit.target.innerText
     console.log(digit.target.innerText)
     displayScreen.querySelector("div").innerText = firstNum;  
-});
-}
+    } else {
+            secondNum = secondNum + digit.target.innerText
+            console.log(digit.target.innerText)
+            displayScreen.querySelector("div").innerText = firstNum + " " + operator + " " + secondNum; 
+        }
+    }
+)};
 
 let operatorSelect = document.querySelectorAll(".op-btn");
 
 for(o of operatorSelect) {
     o.addEventListener('click', function(opt) {
         opt.stopPropagation()  
+        if(operator === "" && firstNum !== "") {
         operator = opt.target.innerText
+        console.log(operator)
         displayScreen.querySelector("div").innerText = firstNum + " " + operator;
-        if(firstNum !== "") {
-            for(i of numPadButtons) {
-                i.addEventListener('click', function(digit) {
-                digit.stopPropagation()   
-                secondNum = secondNum + digit.target.innerText
-                console.log(digit.target.innerText)
-                displayScreen.querySelector("div").innerText = firstNum + " " + operator + " " + secondNum; 
-            });
-            }
-        }
-    })
+    }
+})
 };
+
+let equalSelect = document.querySelector(".eq-btn")
+equalSelect.addEventListener('click', function(e) {
+    e.stopPropagation()
+    console.log("Equals is pressed")
+    if(secondNum !== "") {
+        if(operator === "+") {
+            addFunction(firstNum, secondNum)
+        } 
+    } 
+})
+
+let clearCalculator = document.querySelector("#clear")
+clearCalculator.addEventListener('click', function(e) {
+    e.stopPropagation()
+    clearCalc()
+})
 
 // question is to start = operation is called when = is pressed after selecting 2nd number in the above loop
 
